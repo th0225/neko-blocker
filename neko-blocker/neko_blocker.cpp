5fda3e3908dfed3ec84a41a18c863d078b198572
TerryHung <terryhung@mail.intai.com.tw>
2025-10-14 15:25:09 +0800

    完成禁用鍵盤功能

--- neko-blocker/neko_blocker.cpp
+++ neko-blocker/neko_blocker.cpp
@@ -1,11 +1,82 @@
 #include "neko_blocker.h"
 
-NekoBlocker::NekoBlocker(QWidget *parent)
-    : QMainWindow(parent)
-{
-    ui.setupUi(this);
+#include <Windows.h>
+#include <qobject.h>
+
+#include "my_push_button.h"
+#include "ui_neko_blocker.h"
+
+bool NekoBlocker::isLocked = false;
+HHOOK NekoBlocker::keyboardHook = NULL;
+
+/// <summary>
+/// 建構子
+/// </summary>
+/// <param name="parent"></param>
+NekoBlocker::NekoBlocker(QWidget *parent) : QMainWindow(parent) {
+  ui.setupUi(this);
+
+  QObject::connect(ui.enableButton, &MyPushButton::toggled, this,
+                   &NekoBlocker::onEnableButtonToggled);
 }
 
-NekoBlocker::~NekoBlocker()
-{}
+/// <summary>
+/// 解構子
+/// </summary>
+NekoBlocker::~NekoBlocker() { uninstallHook(); }
 
+/// <summary>
+/// 鍵盤掛鉤處理函數
+/// </summary>
+/// <param name="nCode"></param>
+/// <param name="wParam"></param>
+/// <param name="lParam"></param>
+/// <returns></returns>
+LRESULT CALLBACK NekoBlocker::keyboardProc(int nCode, WPARAM wParam,
+                                           LPARAM lParam) {
+  if (nCode >= 0 && isLocked) {
+    return 1;
+  }
+
+  return CallNextHookEx(keyboardHook, nCode, wParam, lParam);
+}
+
+/// <summary>
+/// 鍵盤上鎖
+/// </summary>
+void NekoBlocker::lock() {
+  if (!keyboardHook) {
+    keyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, keyboardProc,
+                                    GetModuleHandle(NULL), 0);
+  }
+
+  isLocked = true;
+}
+
+/// <summary>
+/// 鍵盤解鎖
+/// </summary>
+void NekoBlocker::unlock() { isLocked = false; }
+
+/// <summary>
+/// 移除鍵盤掛鉤
+/// </summary>
+void NekoBlocker::uninstallHook() {
+  if (isLocked) {
+    UnhookWindowsHookEx(keyboardHook);
+    keyboardHook = NULL;
+  }
+}
+
+/// <summary>
+/// 觸發按鈕啟用/停用事件
+/// </summary>
+void NekoBlocker::onEnableButtonToggled() {
+  if (ui.enableButton->isChecked()) {
+    lock();
+  } else {
+    unlock();
+  }
+
+  ui.enableButton->setText(isLocked ? tr("Disable") : tr("Enable"));
+}
--- neko-blocker/neko_blocker.h
+++ neko-blocker/neko_blocker.h
@@ -1,17 +1,37 @@
 #pragma once
 
+#include <Windows.h>
+#include <qwidget.h>
+
 #include <QtWidgets/QMainWindow>
+
 #include "ui_neko_blocker.h"
 
-class NekoBlocker : public QMainWindow
-{
-    Q_OBJECT
+class NekoBlocker : public QMainWindow {
+  Q_OBJECT
 
-public:
-    NekoBlocker(QWidget *parent = nullptr);
-    ~NekoBlocker();
+ public:
+  // 建構子
+  NekoBlocker(QWidget *parent = nullptr);
+  // 解構子
+  ~NekoBlocker();
 
-private:
-    Ui::NekoBlockerClass ui;
+ private slots:
+  // 觸發按鈕啟用/停用事件
+  void onEnableButtonToggled();
+
+ private:
+  // 鍵盤掛鉤處理函數
+  static LRESULT CALLBACK keyboardProc(int nCode, WPARAM wParam, LPARAM lParam);
+  // 鍵盤上鎖
+  void lock();
+  // 鍵盤解鎖
+  void unlock();
+  // 移除鍵盤掛鉤
+  void uninstallHook();
+
+ private:
+  Ui::NekoBlockerClass ui;
+  static bool isLocked;
+  static HHOOK keyboardHook;
 };
-
