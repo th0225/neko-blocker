35df6325115801d79526fa118d5fe98be7408ab5
TerryHung <terryhung@mail.intai.com.tw>
2025-10-15 08:48:14 +0800

    避免程式重複開啟

--- neko-blocker/main.cpp
+++ neko-blocker/main.cpp
@@ -1,9 +1,21 @@
+#include <qmessagebox.h>
+#include <qsharedmemory.h>
+
 #include <QtWidgets/QApplication>
 
 #include "neko_blocker.h"
 
 int main(int argc, char* argv[]) {
   QApplication app(argc, argv);
+
+  // 避免程式重複開啟
+  QSharedMemory sharedMemory("neko-blocker-unique-key");
+  if (!sharedMemory.create(1)) {
+    QMessageBox::warning(nullptr, "Neko Blocker",
+                         "Another instance is already running.");
+    return 0;
+  }
+
   NekoBlocker window;
 
   return app.exec();
